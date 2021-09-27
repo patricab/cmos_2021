8691fedf4555446ca0b893045426fa8425aac947
patricab <patric.berthelsen@gmail.com>
2021-09-27 16:50:54 +0200

    Refactored mux (demux) and finished 4x8 buffer

--- src/common.sv
+++ src/common.sv
@@ -1,14 +1,38 @@
+`ifndef PACKAGE_COMMON
+`define PACKAGE_COMMON
+
 package common
-    // 8 bit 2-1 Multiplexer (MUX)
-    module 8mux2_1 (
-        input [7:0] in1,
-        input [7:0] in2,
+    // 8 bit 2-1 Multiplexer (MUX) with enable
+    module dmux1_2 (
+        input [7:0] in,
         input sel,
-        output [7:0] out
+        input enable,
+        output [7:0] out1,
+        output [7:0] out2
     );
-    // Switch output based on sel signal
-    // 0: in1, 1: in2
-    out = sel ? in2 : in1; 
+    // Switch which output based on sel signal
+    always @(sel, enable) begin
+        if (enable == 1'b1) begin
+            case (sel)
+                1'b0: begin
+                    out1 <= in;
+                    out2 <= 8'h0;
+                end
+                1'b1: begin
+                    out1 <= 8'h0;
+                    out2 <= in;
+                end
+            endcase
+        end else begin
+            out1 <= 8'h0;
+            out2 <= 8'h0;
+        end
+    end
     endmodule
 
+    //new code
 endpackage
+
+import common::*
+
+`endif
